f9d8dfe1777860220427a5d37ae5044b87874ee2
Ken-yoshi <haruru20170928@gmail.com>
2020-10-18 18:57:09 +0900

    ジャンケンアプリ音声変更

--- jan/ViewController.swift
+++ jan/ViewController.swift
@@ -19,7 +19,14 @@ class ViewController: UIViewController {
 
     var shuffleAction = Bundle.main.bundleURL.appendingPathComponent("audio_16383.wav")
     
+    var winAction = Bundle.main.bundleURL.appendingPathComponent("win.m4a")
+    
+    var loseAction =
+        Bundle.main.bundleURL.appendingPathComponent("lost.m4a")
+    
     var shufflePlayer = AVAudioPlayer()
+    var winPlayer = AVAudioPlayer()
+    var losePlayer = AVAudioPlayer()
     
     @IBOutlet weak var answerImageView: UIImageView!
     
@@ -27,9 +34,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
     
     var answerNumber = 0
+    var jugeNumber = 0
     
     @IBAction func shuffleAction(_ sender: Any) {
        
+        //ジャンケンの声かけ
         do {
             shufflePlayer = try AVAudioPlayer(contentsOf: shuffleAction, fileTypeHint: nil)
             
@@ -37,40 +46,91 @@ class ViewController: UIViewController {
         } catch {
             print("音声エラー")
         }
+        
+        //ジャンケンの手をランダムで変更するための乱数
         var newAnswerNumber = 0
-        
-        repeat{
-            newAnswerNumber = Int.random(in:0..<3)
-        }while
-            newAnswerNumber == self.answerNumber
-        
+        newAnswerNumber = Int.random(in:0..<3)
          answerNumber = newAnswerNumber
+        
+        //勝ち負けをランダムで変更するための乱数
+        var newjugeNumber = 0
+        newjugeNumber = Int.random(in:0..<2)
+         jugeNumber = newjugeNumber
+        
         //ジャンケンポンの掛け声と同時に手を出す
         if answerNumber == 0 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.9) {
                 self.answerLabel.text = "グー"
                 self.answerImageView.image = UIImage(named:"gu")
+                
+                if self.jugeNumber == 0 {
+                    
+                    do {
+                        self.winPlayer = try AVAudioPlayer(contentsOf: self.winAction, fileTypeHint: nil)
+                        
+                        self.winPlayer.play()
+                    } catch {
+                        print("音声エラー")
+                    }
+                } else {
+                    do {
+                        self.losePlayer = try AVAudioPlayer(contentsOf: self.loseAction, fileTypeHint: nil)
+                        
+                        self.losePlayer.play()
+                    } catch {
+                        print("音声エラー")
+                    }
+                }
             }
+            
         } else if answerNumber == 1 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.9) {
                 self.answerLabel.text = "チョキ"
                 self.answerImageView.image = UIImage(named:"choki")
+                
+                if self.jugeNumber == 0 {
+                    
+                    do {
+                        self.winPlayer = try AVAudioPlayer(contentsOf: self.winAction, fileTypeHint: nil)
+                        
+                        self.winPlayer.play()
+                    } catch {
+                        print("音声エラー")
+                    }
+                } else {
+                    do {
+                        self.losePlayer = try AVAudioPlayer(contentsOf: self.loseAction, fileTypeHint: nil)
+                        
+                        self.losePlayer.play()
+                    } catch {
+                        print("音声エラー")
+                    }
+                }
             }
         } else if answerNumber == 2 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.9) {
                 self.answerLabel.text = "パー"
                 self.answerImageView.image = UIImage(named:"pa")
+                
+                if self.jugeNumber == 0 {
+                    do {
+                        self.winPlayer = try AVAudioPlayer(contentsOf: self.winAction, fileTypeHint: nil)
+                        
+                        self.winPlayer.play()
+                    } catch {
+                        print("音声エラー")
+                    }
+                } else {
+                    do {
+                        self.losePlayer = try AVAudioPlayer(contentsOf: self.loseAction, fileTypeHint: nil)
+                        
+                        self.losePlayer.play()
+                    } catch {
+                        print("音声エラー")
+                    }
+                }
             }
         }
-        
-        
-        
     }
-    
-    
-        
-       
-
-       
 }
 
